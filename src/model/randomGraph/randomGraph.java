000c53bfa241b8746752097a3b61cc363db89f88
Melania Ghelli <marcylu@hotmail.it>
2017-05-25 10:56:41 +0200

    Update randomGraph.java

--- src/model/randomGraph/randomGraph.java
+++ src/model/randomGraph/randomGraph.java
@@ -26,8 +26,6 @@ public class randomGraph<T extends Comparable<T>> extends Graph<T>{
 		int n = 10 ;
 		int m = random.nextInt(n) ;
 		
-		System.out.println("Il numero di nodi è " + m );
-	 	
 		for (i=0;i<m;i++) {
 			
 			Node<Integer> x = new Node<Integer>(i) ;
@@ -35,60 +33,53 @@ public class randomGraph<T extends Comparable<T>> extends Graph<T>{
 			this.G.insertNode(x);
 			this.nodesNumber ++ ;
 		}
-		
-		this.G.print() ;
 	}
 		
 	public void randomEdges(){
 		
-		Integer[] A = new Integer[this.nodesNumber] ;
+	Integer[] A = new Integer[this.nodesNumber] ;
 		
         Iterator<Node<Integer>> I = this.G.V().iterator() ;
 		
-	    Integer i = 0 ;
+	Integer i = 0 ;
 	    
 	    
-	    while (I.hasNext()) {
-	    	
-	    	A[i] = I.next().getElement() ;
+	while (I.hasNext()) {
+		
+		A[i] = I.next().getElement() ;
 	    	i++ ;
 	    	
-	    }
+	}
 	    
 	   
-		Node<Integer> u ;
-		Node<Integer> v ;
+	Node<Integer> u ;
+	Node<Integer> v ;
 		
-		Random random1 = new Random() ;
+	Random random1 = new Random() ;
 		
-		if (this.nodesNumber > 1) {
+	if (this.nodesNumber > 1) {
 			
-			int m = this.nodesNumber ;		//MAX_N edges
+		int m = this.nodesNumber ;		//MAX_N edges
 			
-			int n = random1.nextInt(m) ;
+		int n = random1.nextInt(m) ;
 			
-			System.out.println("Il numero di archi è " + n );
-			
-			
-	        for (i=0;i<n;i++){
+		for (i=0;i<n;i++){
 				
-				u = (Node<Integer>) new Node(A[random1.nextInt(this.nodesNumber - 1)]) ;
-				v = (Node<Integer>) new Node(A[random1.nextInt(this.nodesNumber - 1)]) ;
+			u = (Node<Integer>) new Node(A[random1.nextInt(this.nodesNumber - 1)]) ;
+			v = (Node<Integer>) new Node(A[random1.nextInt(this.nodesNumber - 1)]) ;
 				
-				//TODO: verificare che l'arco non sia stato già creato
-				if (u.getElement() != v.getElement()) {
-					this.G.insertEdge(u, v) ;
-					System.out.println("è stato appena creato un arco da " + u + " a " + v );
-				}
-				
-				
-				
+			//TODO: verificare che l'arco non sia stato già creato
+			if (u.getElement() != v.getElement()) {
+				this.G.insertEdge(u, v) ;
+				System.out.println("è stato appena creato un arco da " + u + " a " + v );
 			}
+			
 		}
 	}
+        }
 
 	
-	//TODO: perqualecazzodimotivo non stampa il grafo
+	//TODO: verificare print()
 	public static void main (String[] args) {
 		
 		randomGraph<Integer> g1 = new randomGraph<Integer>() ;
