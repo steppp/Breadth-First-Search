160e5f1ecae0545fc3cedc9056c682aecc18e68b
melastone <marcylu@hotmail.it>
2017-06-09 12:14:37 +0200

    Eliminato ManipulateJSONFile.java
    File ManipulateJSONFile.java unito a ReadJSONFile.java
    Commenti tradotti tutti in italiano. 
    Migliorato il codice di ReadJSONFile.java.

--- src/model/JSONFiles/ReadJSONFile.java
+++ src/model/JSONFiles/ReadJSONFile.java
@@ -2,19 +2,21 @@ package model.JSONFiles ;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.Map ;
+import java.util.Set ;
+import java.util.Iterator ;
 import java.io.IOException;
+import model.graphs.*;
 import model.JSONFiles.exceptions.*;
 import org.json.simple.*;
 import org.json.simple.parser.*;
 
 /* Questo file permette di leggere un file con estensione .json 
    contentente l'implementazione di un grafo. 
-   
-   
    dati caricati da un file di testo
-   strutturato secondo regole stabilite dall’applicazione
-   ALGA verifica se il file è valido ed è possibile caricare i dati; in caso
-   contrario notifica all’utente la presenza di errori
+    •  strutturato secondo regole stabilite dall’applicazione
+    •  ALGA verifica se il file è valido ed è possibile caricare i dati; in caso
+       contrario notifica all’utente la presenza di errori
 */
 
 public class ReadJSONFile {
@@ -23,8 +25,8 @@ public class ReadJSONFile {
 	JSONParser parser = new JSONParser();
 	
 	/*
-	 * Constructor. 
-	 * Memorizes the file path in a local parameter.
+	 * Costruttore.
+	 * Memorizza il percorso del file da prendere in input.
 	 */
 	public ReadJSONFile(String filePath) throws WrongFileExtension,EmptyFileName {
 		
@@ -40,8 +42,8 @@ public class ReadJSONFile {
 	}
 	
     /*
-     * This method returns the JSON Object contained in the file.
-     * Use only if the .json file contains one object.	
+     * Questo metodo ritorna il JSONObject contenuto nel file.
+     * Assumiamo che il file con estensione .json contenga un unico oggetto.
      */
 	public JSONObject getFile () {
 		
@@ -75,8 +77,8 @@ public class ReadJSONFile {
 	}
 	
 	/*
-	 * This method opens an input file stream, reads it content
-	 * and returns a String with the same content.
+	 * Questo metodo apre lo stream con il file di input, ne legge 
+	 * e copia il contenuto in una stringa che viene restituita dallo stesso.
 	 * 
 	 */
 	public String getFileContent() {
@@ -109,6 +111,39 @@ public class ReadJSONFile {
 		return FileC ;
 		
 	}
+	
+
+	/*
+	 * Questo metodo restituisce un grafo a partire da un JSONObject.
+	 * Solleva un'eccezione nel caso in cui l'oggetto non rispetti le regole sintattiche
+	 * notificate dal software.
+	 */
+	public Graph<Integer> readGraph(JSONObject object) throws InvalidFileObject {
+		
+		Graph<Integer> G = new Graph<Integer>() ;
+		
+		if (object.isEmpty() || object.keySet().size() != 2) {		//se l'oggetto è vuoto oppure non ha esattamente due chiavi
+			throw new InvalidFileObject() ;
+			
+		}
+		
+		if(!object.containsKey("vl") || !object.containsKey("el")) {		//se non ci sono entrambe le chiavi vl ed el
+			throw new InvalidFileObject() ;
+		}
+		
+		//TODO: type check - try/catch which exception?
+		JSONObject verticesList = (JSONObject) object.get("vl") ;
+		JSONObject edgesList = (JSONObject) object.get("el") ;
+		
+		if (verticesList.isEmpty() || edgesList.isEmpty()) {		//se l'insieme dei nodi o quello degli archi sono vuoti
+			throw new InvalidFileObject() ;
+		}
+		
+		
+		
+		return G ;
+	} 
+	
 	
 }
 	
--- src/model/randomGraph/randomGraph.java
+++ src/model/randomGraph/randomGraph.java
@@ -5,21 +5,22 @@ import java.util.Iterator;
 import model.graphs.Graph;
 import model.graphs.Node;
 
-//TODO:parametrizza
-//TODO:verificare perchè AlreadyExist restituisce sempre false 
+
 public class randomGraph<T extends Comparable<T>> extends Graph<T>{
 	
-	Graph<Integer> G ;
+	Graph<T> G ;
 	Integer nodesNumber ;
 	
 
 	public randomGraph() {
-		this.G = new Graph<Integer>() ;	
+		this.G = new Graph<T>() ;	
 		this.nodesNumber = 0 ;
 	}
 		
-	//TODO: escludere il caso in cui ci sono 0 nodi
-	//Crea un numero casuale di nodi (MAX 10)	
+	/*
+	 * Crea un numero casuale di nodi (MAX 10) etichettati con numeri interi da 0 a 10	
+	 * Assumiamo che venga applicato ad un grafo di Interi
+	 */
 	public void randomNodes() {
 		
 		Random random = new Random() ;
@@ -32,16 +33,22 @@ public class randomGraph<T extends Comparable<T>> extends Graph<T>{
 			m = random.nextInt(n) ;
 		}
 		
+		
 		for (i=0;i<m;i++) {
 			
-			Node<Integer> x = new Node<Integer>(i) ;
+			Node<T> x = new Node((T)i) ;
 		
 			this.G.insertNode(x);
 			this.nodesNumber ++ ;
+			
 		}
 		
 	}
 		
+	/*
+	 * Crea un numero casuale di archi(MAX 2n)
+	 * Assumiamo che venga applicato ad un grafo di Interi
+	 */
 	public void randomEdges(){
 		
 		Random random1 = new Random() ;
@@ -51,83 +58,28 @@ public class randomGraph<T extends Comparable<T>> extends Graph<T>{
 			
 			int n = this.nodesNumber +  random1.nextInt(this.nodesNumber) ;
 			
-			while (n == 0) {
-				n = random1.nextInt(this.nodesNumber) ;
-			}
-			
-			Integer[] nodesEl = getNodesArray() ;
+			Object[] nodesEl =  this.G.V().toArray() ; ;
 			
 			Integer i ;
 			
-			
 		    for (i=0;i<n;i++){
 		            
-		            Node<Integer> u = (Node<Integer>) new Node(nodesEl[random1.nextInt(this.nodesNumber - 1)]) ;
-					Node<Integer> v = (Node<Integer>) new Node(nodesEl[random1.nextInt(this.nodesNumber - 1)]) ;
-					
-					if (u.getElement() == v.getElement()) {
-						
-						while(u.getElement() == v.getElement()) {
-							
-							u = (Node<Integer>) new Node(nodesEl[random1.nextInt(this.nodesNumber - 1)]) ;
-							v = (Node<Integer>) new Node(nodesEl[random1.nextInt(this.nodesNumber - 1)]) ;
-							
-
-					     }
-					}
-					
-					if (alreadyExist(u,v)) {
-						
-						Integer tmp = u.getElement() ;
-						u = new Node(v.getElement()) ;
-						v = new Node(tmp) ;
-						
-					}
-					
-					if (!alreadyExist(u,v)) {
-						
-						this.G.insertEdge(u, v) ;
-						
-			        }
+		    	Node<T> u = (Node<T>)nodesEl[random1.nextInt(this.nodesNumber - 1)] ;
+		    	Node<T> v = (Node<T>)nodesEl[random1.nextInt(this.nodesNumber - 1)] ;
+		    	
+		    	this.G.insertEdge(u, v) ;
+		    	
 		    }
 		}
 	}
-
 	
-	public Boolean alreadyExist(Node<Integer> x, Node<Integer> y) {
-		
-		Iterator<Node<Integer>> I = this.G.adj(x).iterator() ;
-		
-		Boolean tmp = false ;
-		
-		while (I.hasNext()) {
-			
-			if (I.next() == y) {
-				tmp = true ;
-			}
-			
-		}
-		
-		return tmp ;
-	}
-	
-	public Integer[] getNodesArray() {
-      
-		Integer[] A = new Integer[this.nodesNumber] ;
-		
-      Iterator<Node<Integer>> I = this.G.V().iterator() ;
-		
-	    Integer i = 0 ;
-	    
-	    while (I.hasNext()) {
-	    	
-	    	A[i] = I.next().getElement() ;
-	    	i++ ;
-	    	
-	    }
-	    
-	    return A ;
+	@Override
+	public void print() {
+		this.G.print();
 	}
 
+	
 }
+
+
 
